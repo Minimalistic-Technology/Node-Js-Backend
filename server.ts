9291b10a73aa61f70231f29010e6bda35e0f8abc
Manan1884 <manan18doshi@gmail.com>
2025-05-03 15:16:57 +0530

    Updated server.ts

--- server.ts
+++ server.ts
@@ -9,5 +9,5 @@ connectDB();
 
 const server = http.createServer(app);
 server.listen(PORT, () => {
-  console.log('Server running on http://localhost:${PORT}');
+  console.log(`Server running on http://localhost:${PORT}`);
 });
