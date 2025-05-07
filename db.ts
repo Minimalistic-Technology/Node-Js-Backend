60f990d025331d9d6baa67369459aabb9a5bdf26
Parth Doshi <parthdoshi480@gmail.com>
2025-05-08 00:16:21 +0530

    Structure

--- db.ts
+++ db.ts
@@ -1,16 +1,16 @@
-const mongoose = require('mongoose');
-require('dotenv').config();
+// const mongoose = require('mongoose');
+// require('dotenv').config();
 
-const connectDB = async () => {
-  try {
-    await mongoose.connect(process.env.MONGO_URI, {
-      dbName: 'goldData',
-    });
-    console.log('MongoDB Connected');
-  } catch (error) {
-    console.error('MongoDB Connection Error:', error);
-    setTimeout(connectDB, 5000); 
-  }
-};
+// const connectDB = async () => {
+//   try {
+//     await mongoose.connect(process.env.MONGO_URI, {
+//       dbName: 'goldData',
+//     });
+//     console.log('MongoDB Connected');
+//   } catch (error) {
+//     console.error('MongoDB Connection Error:', error);
+//     setTimeout(connectDB, 5000); 
+//   }
+// };
 
-module.exports = connectDB;
+// module.exports = connectDB;
