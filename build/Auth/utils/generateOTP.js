5d497f99e89e87a63feeca6d8fe84c961ba04991
Manan1884 <manan18doshi@gmail.com>
2025-07-17 00:06:25 +0530

    Delete the build files

--- build/Auth/utils/generateOTP.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function generateOtp(length = 6) {
-    return Math.floor(100000 + Math.random() * 900000)
-        .toString()
-        .slice(0, length);
-}
-exports.default = generateOtp;
--- build/Stocks/route/productToolRoutes.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const productToolController_1 = require("../../controllers/stocks/productToolController");
-router.post('/tools', productToolController_1.addProductTool);
-router.get('/tools', productToolController_1.getAllProductTools);
-router.get('/tools/:id', productToolController_1.getProductToolById);
-router.put('/tools/:id', productToolController_1.updateProductTool);
-router.delete('/tools/:id', productToolController_1.deleteProductTool);
-exports.default = router;
